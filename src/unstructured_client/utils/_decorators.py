260412f3ec64c714c6b7346610c739a9959c7f11
John <43506685+Coniferish@users.noreply.github.com>
2024-02-06 10:31:37 -0600

    modify decorator so it is only raised on 401 error

--- src/unstructured_client/utils/_decorators.py
+++ src/unstructured_client/utils/_decorators.py
@@ -58,10 +58,12 @@ def suggest_defining_url_if_401(func: Callable[_P, operations.PartitionResponse]
     def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> operations.PartitionResponse:
         try:
             return func(*args, **kwargs)
-        except errors.SDKError:
-            general_obj: General = args[0] # type: ignore
-            if not general_obj.sdk_configuration.server_url:
-                warnings.warn("If intending to use the paid API, please define `server_url` in your request.")
+        except errors.SDKError as e:
+            if e.status_code == 401:
+                general_obj: General = args[0] # type: ignore
+                if not general_obj.sdk_configuration.server_url:
+                    warnings.warn("If intending to use the paid API, please define `server_url` in your request.")
+            
             return func(*args, **kwargs)
 
     return wrapper
