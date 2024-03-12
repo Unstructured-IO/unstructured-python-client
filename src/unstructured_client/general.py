9591a9793e600a05cb91e55e6500425fee35f9b2
John <43506685+Coniferish@users.noreply.github.com>
2024-03-12 14:19:26 -0500

    Update general.py

--- src/unstructured_client/general.py
+++ src/unstructured_client/general.py
@@ -15,7 +15,8 @@ class General:
     def __init__(self, sdk_config: SDKConfiguration) -> None:
         self.sdk_configuration = sdk_config
 
-    @suggest_defining_url_if_401 # human code
+    
+    @suggest_defining_url_if_401  # human code
     @handle_split_pdf_page  # human code
     def partition(self, request: Optional[shared.PartitionParameters], retries: Optional[utils.RetryConfig] = None) -> operations.PartitionResponse:
         r"""Pipeline 1"""
