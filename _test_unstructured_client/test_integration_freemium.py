23c1f21e8e6378191658c67fb9283759c27fa46e
Yao You <theyaoyou@gmail.com>
2024-05-10 12:12:12 -0500

    Ci/add integration test with freemium api (#84)
    This PR adds:
    
    - integration test for the python client calling freemium API (hosted on
    aws) to confirm the client payload can be properly parsed by the API
    service and return expected results
    - setup ci to run integration test
    
    Followups will be needed to fully test different combinations of api
    parameters and document types.

---
+++ _test_unstructured_client/test_integration_freemium.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+import requests
+
+import pytest
+
+from unstructured_client import UnstructuredClient
+from unstructured_client.models import shared
+
+
+@pytest.fixture(scope="module")
+def client() -> UnstructuredClient:
+    _client = UnstructuredClient(api_key_auth=os.getenv("UNSTRUCTURED_API_KEY"))
+    yield _client
+
+
+@pytest.fixture(scope="module")
+def doc_path() -> Path:
+    return Path(__file__).resolve().parent.parent / "_sample_docs"
+
+
+@pytest.mark.parametrize("strategy", ["fast", "ocr_only", "hi_res"])
+def test_partition_strategies(strategy, client, doc_path):
+    filename = "layout-parser-paper-fast.pdf"
+    with open(doc_path / filename, "rb") as f:
+        files = shared.Files(
+            content=f.read(),
+            file_name=filename,
+        )
+
+    req = shared.PartitionParameters(
+        files=files,
+        strategy=strategy,
+        languages=["eng"],
+    )
+
+    response = client.general.partition(req)
+    assert response.status_code == 200
+    assert len(response.elements)
+    
