ee68ca4509f9e9b3a734df376b628bb0258c2e4d
Yao You <yao@unstructured.io>
2024-05-22 17:43:48 -0500

    feat: use threadpool
    - to allow Windows to run pdf splitting with the sdk we switch from
      Process based to Thread based multiprocessing
    - add integration test to verify the client can handle pdf splitting
      with successful server response and unsuccesful responses

--- _test_unstructured_client/integration/test_integration_freemium.py
+++ _test_unstructured_client/integration/test_integration_freemium.py
@@ -5,6 +5,8 @@ import pytest
 import requests
 from unstructured_client import UnstructuredClient
 from unstructured_client.models import shared
+from unstructured_client.utils.retries import RetryConfig, BackoffStrategy
+from unstructured_client.models.errors.sdkerror import SDKError
 
 
 @pytest.fixture(scope="module")
@@ -18,8 +20,9 @@ def doc_path() -> Path:
     return Path(__file__).resolve().parents[2] / "_sample_docs"
 
 
+@pytest.mark.parametrize("split_pdf", [True, False])
 @pytest.mark.parametrize("strategy", ["fast", "ocr_only", "hi_res"])
-def test_partition_strategies(strategy, client, doc_path):
+def test_partition_strategies(split_pdf, strategy, client, doc_path):
     filename = "layout-parser-paper-fast.pdf"
     with open(doc_path / filename, "rb") as f:
         files = shared.Files(
@@ -31,8 +34,43 @@ def test_partition_strategies(strategy, client, doc_path):
         files=files,
         strategy=strategy,
         languages=["eng"],
+        split_pdf_page=split_pdf,
     )
 
     response = client.general.partition(req)
     assert response.status_code == 200
     assert len(response.elements)
+
+
+@pytest.mark.parametrize("split_pdf", [True, False])
+@pytest.mark.parametrize("error_code", [500, 403])
+def test_partition_handling_server_error(error_code, split_pdf,monkeypatch, doc_path):
+    filename = "layout-parser-paper-fast.pdf"
+    from unstructured_client.sdkconfiguration import requests_http
+
+    response = requests_http.Response()
+    response.status_code = error_code
+    monkeypatch.setattr(requests_http.Session, "send", lambda *args, **kwargs: response)
+
+    # initialize client after patching
+    client = UnstructuredClient(
+        api_key_auth=os.getenv("UNSTRUCTURED_API_KEY"),
+        retry_config=RetryConfig("backoff", BackoffStrategy(1, 10, 1.5, 30), False),
+    )
+
+    with open(doc_path / filename, "rb") as f:
+        files = shared.Files(
+            content=f.read(),
+            file_name=filename,
+        )
+
+    req = shared.PartitionParameters(
+        files=files,
+        strategy="fast",
+        languages=["eng"],
+        split_pdf_page=split_pdf,
+    )
+
+    with pytest.raises(SDKError, match=f"API error occurred: Status {error_code}"):
+        response = client.general.partition(req)
+
