144e5789954dcc67d2b46e356c4784e27ca8edb3
Austin Walker <austin@unstructured.io>
2025-03-03 12:09:24 -0500

    Reset to 0.30.6

--- src/unstructured_client/_version.py
+++ src/unstructured_client/_version.py
@@ -3,10 +3,10 @@
 import importlib.metadata
 
 __title__: str = "unstructured-client"
-__version__: str = "0.30.7"
+__version__: str = "0.30.6"
 __openapi_doc_version__: str = "1.0.78"
 __gen_version__: str = "2.512.0"
-__user_agent__: str = "speakeasy-sdk/python 0.30.7 2.512.0 1.0.78 unstructured-client"
+__user_agent__: str = "speakeasy-sdk/python 0.30.6 2.512.0 1.0.78 unstructured-client"
 
 try:
     if __package__ is not None:
