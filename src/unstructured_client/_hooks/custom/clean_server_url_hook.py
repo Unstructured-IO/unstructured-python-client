704666ef2cf78943858180c4380125c5e13f2d25
Yuming Long <yuming@unstructured.io>
2025-04-21 10:27:09 -0700

    no choose

--- src/unstructured_client/_hooks/custom/clean_server_url_hook.py
+++ src/unstructured_client/_hooks/custom/clean_server_url_hook.py
@@ -33,33 +33,6 @@ def clean_server_url(base_url: str) -> str:
     
 
 
-def choose_server_url(endpoint_url: str | None, client_url: str, default_endpoint_url: str) -> str:
-    """
-    Helper function to fix a breaking change in the SDK past 0.30.0.
-    When we merged the partition and platform specs, the client server_url stopped working,
-    and users need to pass it in the endpoint function.
-    For now, call this helper in the generated code to set the correct url.
-
-    Order of choices:
-    Endpoint server_url -> s.general.partition(server_url=...)
-      (Passed in as None if not set)
-
-    Base client server_url -> s = UnstructuredClient(server_url=...)
-      (Passed as empty string if not set)
-
-    Default endpoint URL as defined in the spec
-    """
-
-    # If the client doesn't get a server_url, it sets a default of platform
-    # This is not always the correct default - we need to make sure default_endpoint_url is used
-    # So, only use the client url if it has been set to something else
-    if client_url == "https://platform.unstructuredapp.io":
-        client_url = ""
-
-    url = endpoint_url if endpoint_url is not None else (client_url or default_endpoint_url)
-    return clean_server_url(url)
-
-
 class CleanServerUrlSDKInitHook(SDKInitHook):
     """Hook fixing common mistakes by users in defining `server_url` in the unstructured-client"""
 
